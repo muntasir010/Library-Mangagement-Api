9a08ab79437a7dca9b0397a18b9843e4ef47e73e
muntasir010 <naeemtasir03@gmail.com>
2025-08-19 20:17:35 +0600

    create book modle

--- src/modules/book/book.interface.ts
+++ src/modules/book/book.interface.ts
@@ -1,17 +1,9 @@
-import { Model } from "mongoose";
-
 export interface IBook {
-  title: string;
-  author: string;
-  genre:
-    | "FICTION"
-    | "NON_FICTION"
-    | "SCIENCE"
-    | "HISTORY"
-    | "BIOGRAPHY"
-    | " FANTASY";
-  isbn: string;
-  description: string;
-  copies: number;
-  available: boolean;
+title: string,
+author: string,
+genre : "FICTION"| "NON_FICTION"| "SCIENCE"| "HISTORY"| "BIOGRAPHY"|" FANTASY",
+isbn : string,
+description : string,
+copies : number,
+available : boolean
 }
---
+++ src/modules/book/book.model.ts
@@ -0,0 +1,53 @@
+import { model, Schema } from "mongoose";
+import { IBook } from "./book.interface";
+
+const bookSchema = new Schema<IBook>(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    author: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    genre: {
+      type: String,
+      trim: true,
+      required: true,
+      enum: {
+        values: ["FICTION","NON_FICTION","SCIENCE","HISTORY","BIOGRAPHY","FANTASY"]
+      }},
+    isbn: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    copies: {
+      type: Number,
+      min: 0,
+      trim: true,
+      required: true,
+    },
+    available: {
+      type: Boolean,
+      trim: true,
+      default: true
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+const Book = model<IBook>("Book", bookSchema);
+export default Book;
