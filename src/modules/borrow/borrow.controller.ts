0989e74ab003edcb620f2da0e3e46a2f9cea08fd
muntasir010 <naeemtasir03@gmail.com>
2025-08-20 20:26:04 +0600

    create borrow book summary

--- src/modules/borrow/borrow.controller.ts
+++ src/modules/borrow/borrow.controller.ts
@@ -18,6 +18,51 @@ const createBorrowBook = async (req: Request, res: Response) => {
   }
 };
 
+const getBorrowSummary = async (req: Request, res: Response) => {
+  try {
+    const summary = await Borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: "$quantity" },
+        },
+      },
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "bookDetails",
+        },
+      },
+      { $unwind: "$bookDetails" },
+      {
+        $project: {
+          _id: 0,
+          book: {
+            title: "$bookDetails.title",
+            isbn: "$bookDetails.isbn",
+          },
+          totalQuantity: 1,
+        },
+      },
+    ]);
+
+    res.json({
+      success: true,
+      message: "Borrowed books summary retrieved successfully",
+      data: summary,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error fetching summary",
+      error,
+    });
+  }
+};
+
 export const borrowController = {
   createBorrowBook,
+  getBorrowSummary,
 };
--- src/modules/borrow/borrow.route.ts
+++ src/modules/borrow/borrow.route.ts
@@ -3,3 +3,4 @@ import { borrowController } from "./borrow.controller";
 
 export const borrowRoute = Router();
 borrowRoute.post("/", borrowController.createBorrowBook);
+borrowRoute.get("/", borrowController.getBorrowSummary);
